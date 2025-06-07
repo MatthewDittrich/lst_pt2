a47cab15216c8be6555e1f3f0c3a17866c2b16bb
MatthewDittrich <matthew.dittrich@ufl.edu>
2025-06-07 16:38:01 -0400

    Changes

--- pt2_python/tools/pt2_methods.py
+++ pt2_python/tools/pt2_methods.py
@@ -1,123 +1,211 @@
 import awkward as ak
 
+
+
 def unique_per_event(array_of_arrays):
     return ak.Array([list(set(x)) for x in array_of_arrays])
 
 
-def ls_pls_matching_simIdx(events, ls_isused_mask, ls_isfake_mask, pls_isused_mask, pls_isfake_mask, key):
-    # Goal of this function is to create masks
-    # First we will create ls masks for having a sim matched fake/real and used/unused pls
-    # Then we will create a pls mask for having a sim matched fake/real and used/unused ls
 
-    if key not in ["ls","pls"]:
-        raise Exception(f"Key {key} is not recognized!")
-    elif (key == "ls"):
-        # Define the LS simIdx
-        ls_simIdx = events.ls_simIdx
-        # Get the filtered pLS simIdx
-        used_real_pls = events.pls_simIdx[pls_isused_mask & (~pls_isfake_mask) & (events.pls_simIdx != -999) & (events.pls_simIdx < ak.num(events.sim_pt))]
-        unused_real_pls = events.pls_simIdx[(~pls_isused_mask) & (~pls_isfake_mask) & (events.pls_simIdx != -999) & (events.pls_simIdx < ak.num(events.sim_pt))]
-        # Construct the masks
-        ls_matches_usedrealpls_mask = []
-        ls_matches_unusedrealpls_mask = []
-        # Loop over the lists
-        # Not broadcasting due to memory issues
-        for ls_list, pls_usedreal_list, pls_unusedreal_list in zip(ls_simIdx, used_real_pls, unused_real_pls):
-            pls_usedreal_set = set(pls_usedreal_list)
-            pls_unusedreal_set = set(pls_unusedreal_list) 
-            ls_matches_usedrealpls_mask.append([a in pls_usedreal_set for a in ls_list])
-            ls_matches_unusedrealpls_mask.append([a in pls_unusedreal_set for a in ls_list])
+def ls_simIdx_matching(events,fake_pls_mask,pls_used_mask,used_key,hard_key):
 
-        # Return the masks
-        return ak.Array(ls_matches_usedrealpls_mask), ak.Array(ls_matches_unusedrealpls_mask)
- 
-    elif (key == "pls"):
-        # Define the pLS simIdx
-        pls_simIdx = events.pls_simIdx
-        # Get the filtered LS simIdx
-        used_real_ls = events.ls_simIdx[ls_isused_mask & (~ls_isfake_mask) & (events.ls_simIdx != -999) & (events.ls_simIdx < ak.num(events.sim_pt))]
-        unused_real_ls = events.ls_simIdx[(~ls_isused_mask) & (~ls_isfake_mask) & (events.ls_simIdx != -999) & (events.ls_simIdx < ak.num(events.sim_pt))]
-        # Construct the masks
-        pls_matches_usedrealls_mask = []
-        pls_matches_unusedrealls_mask = []
-        # Loop over the lists
-        # Not broadcasting due to memory issues
-        for pls_list, ls_usedreal_list, ls_unusedreal_list in zip(pls_simIdx, used_real_ls, unused_real_ls):
-            ls_usedreal_set = set(ls_usedreal_list)
-            ls_unusedreal_set = set(ls_unusedreal_list) 
-            pls_matches_usedrealls_mask.append([a in ls_usedreal_set for a in pls_list])
-            pls_matches_unusedrealls_mask.append([a in ls_unusedreal_set for a in pls_list])
+    if hard_key not in ["hard", "pu"]:
+        raise Exception(f"Key {hard_key} is not recognized!")
+    if used_key not in ["used","unused"]:
+        raise Exception(f"Key {used_key} is not recognized!")
+    # Define the ls_simIdx
+    ls_simIdx = events.ls_simIdx
+    # Get filtered pls_simIdx list
+    # Filters are dependent on keys
+    if (hard_key == "hard") and (used_key == "used"):
+        pls_simIdx = events.pls_simIdx[pls_used_mask & (~fake_pls_mask) & (events.pls_simIdx != -999) & (events.pls_simIdx < ak.num(events.sim_pt))]
+    elif (hard_key == "hard") and (used_key == "unused"):
+        pls_simIdx = events.pls_simIdx[(~pls_used_mask) & (~fake_pls_mask) & (events.pls_simIdx != -999) & (events.pls_simIdx < ak.num(events.sim_pt))]
+    elif (hard_key == "pu") and (used_key == "used"):
+        pls_simIdx = events.pls_simIdx[pls_used_mask & (~fake_pls_mask) & (events.pls_simIdx != -999) & (events.pls_simIdx >= ak.num(events.sim_pt))]
+    elif (hard_key == "pu") and (used_key == "unused"):
+        pls_simIdx = events.pls_simIdx[(~pls_used_mask) & (~fake_pls_mask) & (events.pls_simIdx != -999) & (events.pls_simIdx >= ak.num(events.sim_pt))]
+    # Construct the mask
+    ls_mask = []
+    for ls_list, pls_list in zip(ls_simIdx, pls_simIdx):
+        pls_set = set(pls_list) 
+        ls_mask.append([a in pls_set for a in ls_list])
 
-        # Return the masks
-        return ak.Array(pls_matches_usedrealls_mask), ak.Array(pls_matches_unusedrealls_mask)
+    # Return the mask
+    return ak.Array(ls_mask)
 
 
-def ls_pls_used_masks(events):
 
-    # Grab Idx for each TC type
-    tc_pls_valid = (events.tc_plsIdx != -999)
-    tc_pt3_valid = (events.tc_pt3Idx != -999)
-    tc_pt5_valid = (events.tc_pt5Idx != -999)
-    tc_t5_valid  = (events.tc_t5Idx != -999)
+def pls_simIdx_matching(events,fake_ls_mask,ls_used_mask,used_key,hard_key):
 
-    # Gather pLS indices used directly in TC
-    pls_direct = events.tc_plsIdx[tc_pls_valid]
+    if hard_key not in ["hard", "pu"]:
+        raise Exception(f"Key {hard_key} is not recognized!")
+    if used_key not in ["used","unused"]:
+        raise Exception(f"Key {used_key} is not recognized!")
+    # Define the pls_simIdx
+    pls_simIdx = events.pls_simIdx
+    # Get filtered ls_simIdx list
+    # Filters are dependent on keys
+    if (hard_key == "hard") and (used_key == "used"):
+        ls_simIdx = events.ls_simIdx[ls_used_mask & (~fake_ls_mask) & (events.ls_simIdx != -999) & (events.ls_simIdx < ak.num(events.sim_pt))]
+    elif (hard_key == "hard") and (used_key == "unused"):
+        ls_simIdx = events.ls_simIdx[(~ls_used_mask) & (~fake_ls_mask) & (events.ls_simIdx != -999) & (events.ls_simIdx < ak.num(events.sim_pt))]
+    elif (hard_key == "pu") and (used_key == "used"):
+        ls_simIdx = events.ls_simIdx[ls_used_mask & (~fake_ls_mask) & (events.ls_simIdx != -999) & (events.ls_simIdx >= ak.num(events.sim_pt))]
+    elif (hard_key == "pu") and (used_key == "unused"):
+        ls_simIdx = events.ls_simIdx[(~ls_used_mask) & (~fake_ls_mask) & (events.ls_simIdx != -999) & (events.ls_simIdx >= ak.num(events.sim_pt))]
+    # Construct the mask
+    pls_mask = []
+    for pls_list, ls_list in zip(pls_simIdx, ls_simIdx):
+        ls_set = set(ls_list) 
+        pls_mask.append([a in ls_set for a in pls_list])
 
-    # pT3: pLS and LS
-    pt3_idx = events.tc_pt3Idx[tc_pt3_valid]
-    pls_from_pt3 = events.pt3_plsIdx[pt3_idx]
-    t3_from_pt3 = events.pt3_t3Idx[pt3_idx]
-    ls0_from_pt3 = events.t3_lsIdx0[t3_from_pt3]
-    ls1_from_pt3 = events.t3_lsIdx1[t3_from_pt3]
+    # Return the mask
+    return ak.Array(pls_mask)
 
-    # pT5: pLS and LS
-    pt5_idx = events.tc_pt5Idx[tc_pt5_valid]
-    pls_from_pt5 = events.pt5_plsIdx[pt5_idx]
-    t5_from_pt5 = events.pt5_t5Idx[pt5_idx]
-    t3_0_from_pt5 = events.t5_t3Idx0[t5_from_pt5]
-    t3_1_from_pt5 = events.t5_t3Idx1[t5_from_pt5]
-    ls0_from_pt5 = events.t3_lsIdx0[t3_0_from_pt5]
-    ls1_from_pt5 = events.t3_lsIdx1[t3_0_from_pt5]
-    ls2_from_pt5 = events.t3_lsIdx0[t3_1_from_pt5]
-    ls3_from_pt5 = events.t3_lsIdx1[t3_1_from_pt5]
 
-    # T5: LS
-    t5_idx = events.tc_t5Idx[tc_t5_valid]
-    t3_0_from_t5 = events.t5_t3Idx0[t5_idx]
-    t3_1_from_t5 = events.t5_t3Idx1[t5_idx]
-    ls0_from_t5 = events.t3_lsIdx0[t3_0_from_t5]
-    ls1_from_t5 = events.t3_lsIdx1[t3_0_from_t5]
-    ls2_from_t5 = events.t3_lsIdx0[t3_1_from_t5]
-    ls3_from_t5 = events.t3_lsIdx1[t3_1_from_t5]
 
-    # Concatenate all the used pLS and LS
-    all_used_pls = ak.concatenate([pls_direct, pls_from_pt3, pls_from_pt5], axis=1)
-    all_used_ls = ak.concatenate([
-        ls0_from_pt3, ls1_from_pt3,
-        ls0_from_pt5, ls1_from_pt5, ls2_from_pt5, ls3_from_pt5,
-        ls0_from_t5, ls1_from_t5, ls2_from_t5, ls3_from_t5,
-    ], axis=1)
+def ls_masks(events, key):
 
-    # Remove duplicates
-    # TODO: Is this needed?
-    unique_used_pls = unique_per_event(all_used_pls)
-    unique_used_ls  = unique_per_event(all_used_ls)
-
-    # Full index arrays of LS and pLS
-    full_pls_idx = ak.local_index(events.pls_pt)
+    # Full index arrays for LS
     full_ls_idx  = ak.local_index(events.ls_pt)
 
-    # Create masks for used LS and pLS
-    pls_mask_list = []
-    for full, used in zip(full_pls_idx, unique_used_pls):
-        used_set = set(used)
-        pls_mask_list.append([idx in used_set for idx in full])
-    pls_mask = ak.Array(pls_mask_list)
+    if key not in ["pt5", "t5", "pt3"]:
+        raise Exception(f"LS Key {key} is not a recognized option!")
 
-    ls_mask_list = []
-    for full, used in zip(full_ls_idx, unique_used_ls):
-        used_set = set(used)
-        ls_mask_list.append([idx in used_set for idx in full])
-    ls_mask = ak.Array(ls_mask_list)
+    elif (key == "pt5"):
+        # Grab the pt5 Idxs used in TCs
+        pt5_idx = events.tc_pt5Idx[events.tc_pt5Idx != -999]
+        # breakdown pt5 into 4 LS
+        t5_from_pt5 = events.pt5_t5Idx[pt5_idx]
+        t3_0_from_pt5 = events.t5_t3Idx0[t5_from_pt5]
+        t3_1_from_pt5 = events.t5_t3Idx1[t5_from_pt5]
+        ls0_from_pt5 = events.t3_lsIdx0[t3_0_from_pt5]
+        ls1_from_pt5 = events.t3_lsIdx1[t3_0_from_pt5]
+        ls2_from_pt5 = events.t3_lsIdx0[t3_1_from_pt5]
+        ls3_from_pt5 = events.t3_lsIdx1[t3_1_from_pt5]
+        # Concatenate 
+        all_pt5_ls = ak.concatenate([
+            ls0_from_pt5, ls1_from_pt5, ls2_from_pt5, ls3_from_pt5,
+        ], axis=1)
+        # Remove duplicates
+        # TODO: Is this needed?
+        unique_pt5_ls = unique_per_event(all_pt5_ls)
+        # Create the Mask
+        ls_from_pt5_mask_list = []
+        for full, pt5 in zip(full_ls_idx, unique_pt5_ls):
+            pt5_set = set(pt5)
+            ls_from_pt5_mask_list.append([idx in pt5_set for idx in full])
+        ls_from_pt5_mask = ak.Array(ls_from_pt5_mask_list)
+        
+        return ls_from_pt5_mask
 
-    return ls_mask, pls_mask
+    elif (key == "t5"):
+        # Grab the t5 Idxs used in TCs
+        t5_idx = events.tc_t5Idx[events.tc_t5Idx != -999]
+        # breakdown t5 into 4 LS
+        t3_0_from_t5 = events.t5_t3Idx0[t5_idx]
+        t3_1_from_t5 = events.t5_t3Idx1[t5_idx]
+        ls0_from_t5 = events.t3_lsIdx0[t3_0_from_t5]
+        ls1_from_t5 = events.t3_lsIdx1[t3_0_from_t5]
+        ls2_from_t5 = events.t3_lsIdx0[t3_1_from_t5]
+        ls3_from_t5 = events.t3_lsIdx1[t3_1_from_t5]
+        # Concatenate 
+        all_t5_ls = ak.concatenate([
+            ls0_from_t5, ls1_from_t5, ls2_from_t5, ls3_from_t5,
+        ], axis=1)
+        # Remove duplicates
+        # TODO: Is this needed?
+        unique_t5_ls = unique_per_event(all_t5_ls)
+        # Create the Mask
+        ls_from_t5_mask_list = []
+        for full, t5 in zip(full_ls_idx, unique_t5_ls):
+            t5_set = set(t5)
+            ls_from_t5_mask_list.append([idx in t5_set for idx in full])
+        ls_from_t5_mask = ak.Array(ls_from_t5_mask_list)
+
+        return ls_from_t5_mask
+
+    elif (key == "pt3"):
+        # Grab the pt3 Idxs used in TCs
+        pt3_idx = events.tc_pt3Idx[events.tc_pt3Idx != -999]
+        # breakdown pt3 into 2 LS
+        t3_from_pt3 = events.pt3_t3Idx[pt3_idx]
+        ls0_from_pt3 = events.t3_lsIdx0[t3_from_pt3]
+        ls1_from_pt3 = events.t3_lsIdx1[t3_from_pt3]
+        # Concatenate 
+        all_pt3_ls = ak.concatenate([
+            ls0_from_pt3, ls1_from_pt3,
+        ], axis=1)
+        # Remove duplicates
+        # TODO: Is this needed?
+        unique_pt3_ls = unique_per_event(all_pt3_ls)
+        # Create the Mask
+        ls_from_pt3_mask_list = []
+        for full, pt3 in zip(full_ls_idx, unique_pt3_ls):
+            pt3_set = set(pt3)
+            ls_from_pt3_mask_list.append([idx in pt3_set for idx in full])
+        ls_from_pt3_mask = ak.Array(ls_from_pt3_mask_list)
+
+        return ls_from_pt3_mask
+
+
+
+
+
+def pls_masks(events, key):
+
+    # Full index arrays for pLS
+    full_pls_idx  = ak.local_index(events.pls_pt)
+
+    if key not in ["pt5", "pLS", "pt3"]:
+        raise Exception(f"Key {key} is not a recognized option!")
+
+    elif (key == "pt5"):
+        # Grab the pt5 Idxs used in TCs
+        pt5_idx = events.tc_pt5Idx[events.tc_pt5Idx != -999]
+        # Grab the pLS from pt5
+        pls_from_pt5 = events.pt5_plsIdx[pt5_idx]
+        # Remove duplicates
+        # TODO: Is this needed?
+        unique_pt5_pls = unique_per_event(pls_from_pt5)
+        # Create the Mask
+        pls_from_pt5_mask_list = []
+        for full, pt5 in zip(full_pls_idx, unique_pt5_pls):
+            pt5_set = set(pt5)
+            pls_from_pt5_mask_list.append([idx in pt5_set for idx in full])
+        pls_from_pt5_mask = ak.Array(pls_from_pt5_mask_list)
+        
+        return pls_from_pt5_mask
+
+    elif (key == "pt3"):
+        # Grab the pt3 Idxs used in TCs
+        pt3_idx = events.tc_pt3Idx[events.tc_pt3Idx != -999]
+        # Grab the pLS from pt3
+        pls_from_pt3 = events.pt3_plsIdx[pt3_idx]
+        # Remove duplicates
+        # TODO: Is this needed?
+        unique_pt3_pls = unique_per_event(pls_from_pt3)
+        # Create the Mask
+        pls_from_pt3_mask_list = []
+        for full, pt3 in zip(full_pls_idx, unique_pt3_pls):
+            pt3_set = set(pt3)
+            pls_from_pt3_mask_list.append([idx in pt3_set for idx in full])
+        pls_from_pt3_mask = ak.Array(pls_from_pt3_mask_list)
+        
+        return pls_from_pt3_mask
+
+    elif (key == "pLS"):
+        # Grab the pLS Idxs used in TCs
+        pls_idx = events.tc_plsIdx[events.tc_plsIdx != -999]
+        # Remove duplicates
+        # TODO: Is this needed?
+        unique_tc_pls = unique_per_event(pls_idx)
+        # Create the Mask
+        pls_from_tc_mask_list = []
+        for full, pls in zip(full_pls_idx, unique_tc_pls):
+            pls_set = set(pls)
+            pls_from_tc_mask_list.append([idx in pls_set for idx in full])
+        pls_from_tc_mask = ak.Array(pls_from_tc_mask_list)
+        
+        return pls_from_tc_mask
