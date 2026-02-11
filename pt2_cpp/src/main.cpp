6e8ba55188ce1428c0600a86bc93ea4242e919fb
MatthewDittrich <matthew.dittrich@ufl.edu>
2026-02-11 15:06:27 -0500

    Correct Pixel Map Path

--- pt2_cpp/src/main.cpp
+++ pt2_cpp/src/main.cpp
@@ -78,7 +78,7 @@ int main(int argc, char** argv) {
 
     // Get the pixelMap loaded
     SuperbinToDetIdMap superbinToDetIds;
-    std::string pixelMapFile = "/cmsuf/data/store/user/t2/users/matthew.dittrich/PT2_DATA/PIXEL_MAPS/pLS_map_ElCheapo.txt";
+    std::string pixelMapFile = "/cmsuf/data/store/user/t2/users/matthew.dittrich/PT2_DATA/PIXEL_MAPS/Standard_0p8GeV_Maps/pLS_map_ElCheapo.txt";
     try {
         loadSuperbinDetIdMap(pixelMapFile, superbinToDetIds);
     } catch (const std::exception& e) {
